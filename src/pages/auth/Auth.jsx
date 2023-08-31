bd73854b94e542d35712fe3a964bad2f4bac0689
MoYuBin <bin8727@naver.com>
2023-08-31 16:11:34 +0900

    feat #10: 회원가입 페이지에서 멘티, 멘토 버튼 생성

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import * as S from './Auth.style';
 import googlePath from '../../assets/google-logo.svg';
 import kakaoPath from '../../assets/kakao-logo.svg';
@@ -5,6 +7,12 @@ import logoPath from '../../assets/login-logo.svg';
 import Button from '../../components/auth-button/Button';
 
 const Auth = () => {
+  const [selectedRole, setSelectedRole] = useState('menti');
+
+  const handleRoleChange = (e) => {
+    setSelectedRole(e.target.value);
+  };
+
   return (
     <S.Container>
       <S.LogoWrap>
@@ -12,6 +20,30 @@ const Auth = () => {
       </S.LogoWrap>
 
       <S.SideWrap>
+        <S.TabsWrapper>
+          <label>
+            <input
+              value="menti"
+              type="radio"
+              checked={selectedRole === 'menti'}
+              onChange={handleRoleChange}
+            />
+            <span>멘티</span>
+          </label>
+
+          <label>
+            <input
+              value="mento"
+              type="radio"
+              checked={selectedRole === 'mento'}
+              onChange={handleRoleChange}
+            />
+            <span>멘토</span>
+          </label>
+
+          <span className="selection"></span>
+        </S.TabsWrapper>
+
         <h1>안녕하세요?</h1>
 
         <p>
--- src/pages/auth/Auth.style.jsx
+++ src/pages/auth/Auth.style.jsx
@@ -35,6 +35,62 @@ export const SideWrap = styled.div`
   }
 `;
 
+export const TabsWrapper = styled.div`
+  input {
+    display: none;
+  }
+
+  position: relative;
+  display: flex;
+  align-items: center;
+  border-radius: 999px;
+  background-color: #fff;
+  width: 100%;
+  overflow: hidden;
+  border: 1px solid rgba(53, 52, 52, 0.226);
+
+  label {
+    width: 100%;
+    padding: 10px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+    font-weight: 600;
+    letter-spacing: -1px;
+    font-size: 14px;
+  }
+
+  .selection {
+    display: none;
+    position: absolute;
+    height: 100%;
+    width: calc(100% / 2);
+    z-index: 0;
+    left: 0;
+    top: 0;
+    transition: 0.15s ease;
+  }
+
+  & label:has(input:checked) {
+    color: #fff;
+  }
+
+  & label:has(input:checked) ~ .selection {
+    background-color: rgb(11 117 223);
+    display: inline-block;
+  }
+
+  & label:nth-child(1):has(input:checked) ~ .selection {
+    transform: translateX(0);
+  }
+
+  & label:nth-child(2):has(input:checked) ~ .selection {
+    transform: translateX(calc(100% * 1));
+  }
+`;
+
 export const ButtonBox = styled.div`
   display: flex;
   justify-content: space-between;
